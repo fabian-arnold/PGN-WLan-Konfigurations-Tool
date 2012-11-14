1976decfdc92dd54ae8b38d9154116ee54f5e87a
Fabian Arnold <fabi.arnold@ci4you.com>
2012-11-14 10:52:42 +0100

    Initial Commit

---
+++ Windows 7/PGN Wlan Tool/PGN-HOTSPOT Login/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace PGN_HOTSPOT_Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 22);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Hotspot suchen...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.comboBox1.Location = new System.Drawing.Point(130, 161);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(252, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(12, 188);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(370, 22);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Konfigurieren";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(10, 217);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(374, 66);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(10, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(374, 148);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(394, 293);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MinimumSize = new System.Drawing.Size(400, 38);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.ShowInTaskbar = false;
+            this.Text = "HOTSPOT - Setup";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Windows 7/PGN Wlan Tool/PGN-HOTSPOT Login/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NativeWifi;
+
+namespace PGN_HOTSPOT_Login
+{
+    public partial class Form1 : Form
+    {
+        WlanClient client;
+        List <Wlan.WlanAvailableNetwork> hosts;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        /// <summary>
+        /// Wandelt den übergebenen String in die Hexadezimaldarstellung
+        /// </summary>
+        /// <param name="Hexstring">der umzuwandelnde String</param>
+        /// <returns>Hexadezimaldarstellung</returns>
+        private string StringToHex(string hexstring)
+        {
+            var sb = new StringBuilder();
+            hexstring.Trim();
+            foreach (char t in hexstring)
+                sb.Append(Convert.ToInt32(t).ToString("x"));
+            return sb.ToString().ToUpper();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (client.Interfaces.Length < 1)
+            {
+                MessageBox.Show("Keine WLan-Gerät verfügbar.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+                return;
+            }
+         /*   foreach (Wlan.WlanProfileInfo profileInfo in client.Interfaces[0].GetProfiles())
+            {
+                string name = profileInfo.profileName; // this is typically the network's SSID
+                string xml = client.Interfaces[0].GetProfileXml(profileInfo.profileName);
+                Clipboard.SetText(xml);
+                MessageBox.Show(xml);
+                
+            }*/
+            button2.Enabled = false;
+            if (client.Interfaces[0].InterfaceState == Wlan.WlanInterfaceState.Disconnected)
+            {
+                client.Interfaces[0].Scan();
+                comboBox1.Items.Clear();
+                hosts.Clear();
+                
+                    
+                foreach (Wlan.WlanAvailableNetwork host in client.Interfaces[0].GetAvailableNetworkList(0))
+                {
+                    if (host.dot11DefaultAuthAlgorithm == Wlan.Dot11AuthAlgorithm.RSNA)
+                    {
+                        string ssid = System.Text.Encoding.UTF8.GetString(host.dot11Ssid.SSID).Remove((int)host.dot11Ssid.SSIDLength);
+                        //MessageBox.Show(StringToHex(ssid));
+                        comboBox1.Items.Add(ssid);
+                        hosts.Add(host);
+                        comboBox1.SelectedIndex = 0;
+                        comboBox1.Enabled = true;
+                        button2.Enabled = true;
+                    }
+                    
+                }
+                
+                
+            }
+            else
+            {
+                comboBox1.Items.Clear();
+                comboBox1.Items.Add("Fehler: Device " + client.Interfaces[0].InterfaceState.ToString());
+                comboBox1.SelectedIndex = 0;
+                comboBox1.Enabled = false;
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new WlanClient();
+            hosts = new List<Wlan.WlanAvailableNetwork>();
+            if (client.Interfaces.Length < 1)
+            {
+                MessageBox.Show("Keine WLan-Gerät verfügbar.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+                return;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (client.Interfaces.Length < 1)
+            {
+                MessageBox.Show("Keine WLan-Gerät verfügbar.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+                return;
+            }
+
+            if (client.Interfaces[0].InterfaceState == Wlan.WlanInterfaceState.Disconnected)
+            {
+
+                string profileName, profileXml;
+                profileName = comboBox1.SelectedItem.ToString();
+                if (profileName.Length > 1)
+                {
+                    profileXml = String.Format(Properties.Resources.Template, profileName, StringToHex(profileName));
+                    client.Interfaces[0].SetProfile(Wlan.WlanProfileFlags.AllUser, profileXml, true);
+                    //client.Interfaces[0].Connect(Wlan.WlanConnectionMode.Auto,Wlan.Dot11BssType.Any, profileName);
+                }
+                else
+                {
+                    MessageBox.Show("Fehler");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Fehler: Device " + client.Interfaces[0].InterfaceState.ToString());
+            }
+            
+            
+        }
+    }
+}
